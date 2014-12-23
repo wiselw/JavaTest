a8c169020fe3100e6e7b7fd5f9be14d8dcee7a64
lw梁卫 <weiliang@ctrip.com>
2014-12-23 10:05:12 +0800

    add

---
+++ JavaExample/src/com/java/core/Test1.java
@@ -0,0 +1,17 @@
+package com.java.core;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a=9;
+        int b=0;
+        if(a>9)
+        	b=1;
+        else 
+			b=-1;
+		
+        System.out.print(b);
+	}
+
+}
---
+++ JavaExample/src/com/jdbc/JDBCDemo.java
@@ -0,0 +1,10 @@
+package com.jdbc;
+
+public class JDBCDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+	}
+
+}
